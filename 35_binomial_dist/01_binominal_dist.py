c353cb2a751393c0ef13abde7a3ae5f44a0ec189
peterhchen <peter.hp.chen@gmail.com>
2020-05-08 16:03:56 -0700

    Binomial Distribution

---
+++ 35_binomial_dist/01_binominal_dist.py
@@ -0,0 +1,13 @@
+from scipy.stats import binom
+import seaborn as sb
+import matplotlib.pyplot as plt
+
+binom.rvs(size=10,n=20,p=0.8)
+
+data_binom = binom.rvs(n=20,p=0.8,loc=0,size=1000)
+ax = sb.distplot(data_binom,
+                  kde=True,
+                  color='blue',
+                  hist_kws={"linewidth": 25,'alpha':1})
+ax.set(xlabel='Binomial', ylabel='Frequency')
+plt.show()
